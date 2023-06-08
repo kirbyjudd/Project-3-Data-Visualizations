13754563802ea750a3134f97e86b37c2a677e019
vdescioli <vdescioli@gmail.com>
2023-06-08 18:38:02 -0400

    adding initial api flask file app.py

--- Project 3/app.py
+++ Project 3/app.py
@@ -19,8 +19,7 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # Save a reference to the cities table as `Cities` and countries table as `Countries`
-Cities = Base.classes.cities
-Countries = Base.classes.aqi_countries
+data = Base.classes.openaq
 
 #################################################
 # Flask Setup
@@ -37,48 +36,48 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/cities<br/>"
-        f"/api/v1.0/countries"
+        f"/api/v1.0/pollutants<br/>"
+        # f"/api/v1.0/countries"
     )
 
 
-@app.route("/api/v1.0/cities")
+@app.route("/api/v1.0/pollutants")
 def cities():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of all city names"""
-    # Query all cities
-    results = session.query(Cities.asciiname).distinct().all()
+    # Query all pollutants
+    results = session.query(data).all()
 
     session.close()
 
     # Convert list of tuples into normal list
-    city_names = list(np.ravel(results))
+    pollutant_names = list(np.ravel(results))
     
-    return jsonify(city_names)
+    return jsonify(pollutant_names)
 
 
-@app.route("/api/v1.0/countries")
-def countries():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+# @app.route("/api/v1.0/countries")
+# def countries():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
 
-    """Return a list of country data including the name and population of each country"""
-    # Query all countries
-    results = session.query(Countries.Country_Region, Countries.Population).all()
+#     """Return a list of country data including the name and population of each country"""
+#     # Query all countries
+#     results = session.query(Countries.Country_Region, Countries.Population).all()
 
-    session.close()
+#     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_countries
-    all_countries = []
-    for Country_Region, Population in results:
-        country_dict = {}
-        country_dict["Country"] = Country_Region
-        country_dict["Population"] = Population
-        all_countries.append(country_dict)
+#     # Create a dictionary from the row data and append to a list of all_countries
+#     all_countries = []
+#     for Country_Region, Population in results:
+#         country_dict = {}
+#         country_dict["Country"] = Country_Region
+#         country_dict["Population"] = Population
+#         all_countries.append(country_dict)
 
-    return jsonify(all_countries)
+#     return jsonify(all_countries)
 
 
 if __name__ == '__main__':
