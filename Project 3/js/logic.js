3f3f7c9cf256ccc1410aa4f06053d55660b7c57d
kirbyjudd <kirbyjudd@ymail.com>
2023-06-10 11:52:13 -0400

    updated the logic.js code

--- Project 3/js/logic.js
+++ Project 3/js/logic.js
@@ -8,36 +8,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let link = "Resources/openaq.geojson";
 // Getting our GeoJSON data
 d3.json(link).then(function(data) {
-    let geojsonMarkerOptions = {
-        radius: 15,
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-    };
+
     function circleColor(value, parameter) {
         if (parameter === "O3" && value >= 0.097) return "red";
-        else if (parameter === "O3" && value >= 0.077 && value < 0.097) return "orange";
-        else if (parameter === "O3" && value >= 0.061 && value < 0.076) return "yellow";
-        else if (parameter === "O3" && value >= 0 && value < 0.06) return "green";
+        else if (parameter === "O3" && value >= 0.077 && value <= 0.097) return "orange";
+        else if (parameter === "O3" && value >= 0.061 && value <= 0.076) return "yellow";
+        else if (parameter === "O3" && value >= 0 && value <= 0.06) return "green";
         else if (parameter === "PM2.5" && value >= 57) return "red";
-        else if (parameter === "PM2.5" && value >= 36 && value < 57) return "orange";
-        else if (parameter === "PM2.5" && value >= 13 && value < 35) return "yellow";
-        else if (parameter === "PM2.5" && value >= 0 && value < 13) return "green";
+        else if (parameter === "PM2.5" && value >= 36 && value <= 57) return "orange";
+        else if (parameter === "PM2.5" && value >= 13 && value <= 35) return "yellow";
+        else if (parameter === "PM2.5" && value >= 0 && value <= 13) return "green";
         else if (parameter === "PM10" && value >= 254) return "red";
-        else if (parameter === "PM10" && value >= 156 && value < 254) return "orange";
-        else if (parameter === "PM10" && value >= 56 && value < 154) return "yellow";
-        else if (parameter === "PM10" && value >= 0 && value < 56) return "green";
+        else if (parameter === "PM10" && value >= 156 && value <= 254) return "orange";
+        else if (parameter === "PM10" && value >= 56 && value <= 154) return "yellow";
+        else if (parameter === "PM10" && value < 56) return "green";
         else return "grey";
     }
-    for (let i = 0; i < data.features.length; i++) {
-        let unit = data.features[i].properties.measurements_unit;
-        let value = data.features[i].properties.measurements_value;
-        let location = data.features[i].properties.location;
-        let city = data.features[i].properties.city;
-        let parameter = data.features[i].properties.measurements_parameter;
-        let updated = data.features[i].properties.measurements_lastupdated;
-    }
+    
     // Creating a GeoJSON layer with the retrieved data
     L.geoJson(data, {
         pointToLayer: function (feature, location) {
